05d2b4fb3a9258b62abd81a6efe654269a529863
pavithramuddu93 <pavithramuddu93>
2022-09-24 07:13:03 +0530

    Welcome message added

---
+++ src/com/bridgelabz/HashTable.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author PAVITHRA C
+ *
+ */
+public class HashTable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to Hah table");
+	}
+
+}
