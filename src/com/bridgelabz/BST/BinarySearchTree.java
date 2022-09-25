0a78c2b73c1cab5c8fbed15ba7645b2d16696c4d
pavithramuddu93 <pavithramuddu93>
2022-09-25 13:27:22 +0530

    Added UC3: Ability to search 63
    in the Binary Tree

--- src/com/bridgelabz/BST/BinarySearchTree.java
+++ src/com/bridgelabz/BST/BinarySearchTree.java
@@ -69,9 +69,10 @@ public class BinarySearchTree {
 			}
 
 			else if (data < parent.data) {
-				System.out.print("The given element " + data + " is the left node of the Parent Node " + parent.data);
+				System.out.println("The given element " + data + " is the left node of the Parent Node " + parent.data);
 			} else {
-				System.out.print("The given element " + data + " is the right node of the Parent Node " + parent.data);
+				System.out
+						.println("The given element " + data + " is the right node of the Parent Node " + parent.data);
 			}
 
 			return;
@@ -86,5 +87,4 @@ public class BinarySearchTree {
 			search(root.right, data, root);
 		}
 	}
-
 }
